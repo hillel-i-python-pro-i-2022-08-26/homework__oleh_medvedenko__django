80f45691f6ef9c2368cf443859d1675866cc6bea
chtholine <oleg123qwe@gmail.com>
2022-10-23 21:46:48 +0300

    added CRUD :wrench:

--- apps/contacts/forms.py
+++ apps/contacts/forms.py
@@ -1,12 +1,31 @@
+from datetime import datetime
+
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 from django import forms
-from django.forms import SelectDateWidget
+from django.urls import reverse_lazy
 
 from apps.contacts.models import Contacts
 
 
 class ContactsForm(forms.ModelForm):
-    date = forms.DateField(widget=SelectDateWidget())
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_action = reverse_lazy("contacts_app:show_contacts")
+        self.helper.form_method = "GET"
+        self.helper.add_input(Submit("submit", "Submit"))
 
     class Meta:
         model = Contacts
-        fields = "__all__"
+        fields = (
+            "name",
+            "phone_number",
+            "birthday",
+        )
+
+        widgets = {
+            "name": forms.TextInput(attrs={"style": "width:220px"}),
+            "phone_number": forms.TextInput(attrs={"style": "width:220px"}),
+            "birthday": forms.DateInput(attrs={"type": "date", "max": datetime.now().date(), "style": "width:220px"}),
+        }
